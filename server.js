97397a4e4c222b8416ee969e90cf6354809f2d2d
freakyjones <52784995+freakyjones@users.noreply.github.com>
2020-05-07 01:41:23 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
-console.log('hello')
+
 const togglebtn=document.querySelector('.toggle_btn')
 const buttons=document.querySelector('.btn_section')
-console.log(buttons)
-const img=document.querySelector('img');
 
 
 togglebtn.addEventListener('click',()=>{

1fe4c0a0eb52cafc0d469d434a78f730393ddf35
Your Name <abhilashpandey8170@gmail.com>
2020-06-02 21:47:55 +0530

    cart page is added

---
+++ cart.js
@@ -0,0 +1,7 @@
+const toggle_btn=document.querySelector('.sandwitch_btn')
+
+const nav_links=document.querySelector('.nav_links');
+
+toggle_btn.addEventListener("click",()=>{
+    nav_links.classList.toggle('toggle')
+})
